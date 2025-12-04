feb8280e066f1f4724f630b584a805476c03caea
Ilya Kochev <kochev0987@gmail.com>
2025-12-04 13:35:56 +0300

    added PlayerInput

--- Assets/_Project/Scripts/Scenes/Game/Unit/Controls/UserInputControls.cs
+++ Assets/_Project/Scripts/Scenes/Game/Unit/Controls/UserInputControls.cs
@@ -1,19 +1,47 @@
 ﻿using System;
-using System.Numerics;
 using UniRx;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = System.Numerics.Vector2;
 
 namespace _Project.Scripts.Scenes.Game.Unit.Controls
 {
-  public class UserInputControls : IInputControls
-  {
-    private readonly Subject<Vector2> _movement = new();
-    private readonly Subject<Vector2> _rotation = new();
-    
-    public IObservable<Vector2> OnMovement => _movement;
-    public IObservable<Vector2> OnRotation => _rotation;
+    public class UserInputControls : IInputControls
+    {
+        private readonly Subject<Vector2> _movement = new();
+        private readonly Subject<Vector2> _rotation = new();
 
+        public IObservable<Vector2> OnMovement => _movement;
+        public IObservable<Vector2> OnRotation => _rotation;
 
-    //_movement.OnNext(directionDelta);
-    //_rotation.OnNext(rotationDelta);
-  }
+        private PlayerControls _input;
+
+        public UserInputControls()
+        {
+            _input = new PlayerControls();
+            _input.Player.Move.performed += OnMove;
+            _input.Player.Move.canceled  += OnMove;
+            
+            _input.Player.Look.performed += OnLook;
+            _input.Player.Look.canceled  += OnLook;
+
+            _input.Enable();
+        }
+
+        private void OnMove(InputAction.CallbackContext ctx)
+        {
+            UnityEngine.Vector2 unityValue = ctx.ReadValue<UnityEngine.Vector2>();
+            var v = new Vector2(unityValue.x, unityValue.y);
+            _movement.OnNext(v);
+        }
+
+        private void OnLook(InputAction.CallbackContext ctx)
+        {
+            UnityEngine.Vector2 unityValue = ctx.ReadValue<UnityEngine.Vector2>();
+            var v = new Vector2(unityValue.x, unityValue.y);
+
+            _rotation.OnNext(v);
+        }
+    }
 }
