59c9f9b1994817fec6a405df000937eb96ab17c8
Ilya Kochev <kochev0987@gmail.com>
2025-12-05 08:36:31 +0300

    added needed interface

--- Assets/_Project/Scripts/Scenes/Game/Unit/Controls/UserInputControls.cs
+++ Assets/_Project/Scripts/Scenes/Game/Unit/Controls/UserInputControls.cs
@@ -3,9 +3,14 @@ using UniRx;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public interface IUserInputControls
+{
+    void EnablePlayerActions();
+}
+
 namespace _Project.Scripts.Scenes.Game.Unit.Controls
 {
-    public class UserInputControls : IInputControls, PlayerControls.IPlayerActions
+    public class UserInputControls : IInputControls, IUserInputControls, PlayerControls.IPlayerActions
     {
         private readonly Subject<Vector2> _movement = new();
         private readonly Subject<Vector2> _rotation = new();
@@ -15,20 +20,16 @@ namespace _Project.Scripts.Scenes.Game.Unit.Controls
 
         private PlayerControls _input;
 
-        public UserInputControls()
-        {
-            _input = new PlayerControls();
-            
-            _input.Player.Move.performed += OnMove;
-            _input.Player.Move.canceled  += OnMove;
-            
-            _input.Player.Look.performed += OnLook;
-            _input.Player.Look.canceled  += OnLook;
-
-            _input.Enable();
-        }
-
         public void OnMove(InputAction.CallbackContext ctx) => _movement.OnNext(ctx.ReadValue<Vector2>());
         public void OnLook(InputAction.CallbackContext ctx)  => _rotation.OnNext(ctx.ReadValue<Vector2>());
+        public void EnablePlayerActions()
+        {
+            if(_input == null)
+            {
+                _input = new PlayerControls();
+                _input.Player.SetCallbacks(this);
+            }
+            _input.Enable();
+        }
     }
 }
