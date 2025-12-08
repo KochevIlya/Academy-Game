3980ce93349d38e1d0d132f2fb34c8205d02c595
Maksim Zabelo <maxi2000138@gmail.com>
2025-12-08 03:29:54 +0300

    added riffle movement animations

--- Assets/_Project/Scripts/Scenes/Game/Unit/Animator/UnitAnimator.cs
+++ Assets/_Project/Scripts/Scenes/Game/Unit/Animator/UnitAnimator.cs
@@ -8,8 +8,17 @@ public class UnitAnimator : MonoBehaviour, IAnimationStateReader
   [SerializeField] private Animator _animator;
         
   public readonly ReactiveCommand<Unit> OnShootCast = new();
+  
+  private Vector2 _lastVelocity = Vector2.zero;
 
-  public void Run(float speed) => _animator.SetFloat(Animations.Velocity, speed);
+  public void Run(Vector2 velocity, float lerpValue)
+  {
+    Vector2 lerpVector = Vector2.Lerp(_lastVelocity, velocity, lerpValue);
+    _animator.SetFloat(Animations.VelocityX, lerpVector.x);
+    _animator.SetFloat(Animations.VelocityY, lerpVector.y);
+    _lastVelocity = lerpVector;
+  }
+  
   public void Shoot() => _animator.SetTrigger(Animations.Shoot);
 
 
--- Assets/_Project/Scripts/Utils/Animations.cs
+++ Assets/_Project/Scripts/Utils/Animations.cs
@@ -4,7 +4,8 @@ namespace _Project.Scripts.Utils
 {
   public static class Animations
   {
-    public static int Velocity => Animator.StringToHash(nameof(Velocity));
+    public static int VelocityX => Animator.StringToHash(nameof(VelocityX));
+    public static int VelocityY => Animator.StringToHash(nameof(VelocityY));
     public static int Shoot => Animator.StringToHash(nameof(Shoot));
   }
 }
