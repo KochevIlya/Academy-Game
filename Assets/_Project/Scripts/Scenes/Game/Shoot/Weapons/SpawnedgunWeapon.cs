b164463f6103d9499ce883fe6c453cf285870691
Ilya Kochev <kochev0987@gmail.com>
2026-03-01 15:18:58 +0300

    Added Brone and Spawnable Bullets

--- Assets/_Project/Scripts/Scenes/Game/Shoot/Weapons/SpawnedgunWeapon.cs
+++ Assets/_Project/Scripts/Scenes/Game/Shoot/Weapons/SpawnedgunWeapon.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using _Project.Scripts.Scenes.Game.Shoot;
+using _Project.Scripts.Scenes.Game.Unit;
 using UnityEngine;
 
 public class SpawnedgunWeapon : RiffleWeapon
 {
+    public override void Shoot(Vector2 shootMousePosition, GameUnit unit)
+    {
+        if (_currentTime >= WeaponData.CoolDown)
+        {
+            float fireHeight = SpawnPoint.position.y;
+            _inputHelper.ScreenToGroundPosition(shootMousePosition, fireHeight, out var worldPosition); 
+            var direction = (worldPosition - SpawnPoint.position).normalized;
+            SpawnAndSetup(direction, WeaponData.Speed, WeaponData.BulletLifeTime, WeaponData.Damage, unit).Forget();
+            _currentTime = 0f;
+        }
+    }
+    
     
 }
