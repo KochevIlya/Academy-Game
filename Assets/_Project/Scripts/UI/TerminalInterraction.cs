f37d1a5949065f02013569cb7f227597b5a8bb47
Ilya Kochev <kochev0987@gmail.com>
2026-04-24 13:07:32 +0300

    Added Terminal_OFF ability

--- Assets/_Project/Scripts/UI/TerminalInterraction.cs
+++ Assets/_Project/Scripts/UI/TerminalInterraction.cs
@@ -4,20 +4,27 @@ using _Project.Scripts.Scenes.Game.Hacking.Terminal;
 using UniRx;
 using UnityEngine;
 
-public class InterractionHide : MonoBehaviour
+public class TerminalInterraction : MonoBehaviour
 {
     [SerializeField] private HackingTerminal _hackingTerminal;
     [SerializeField] private GameObject _objectToToggle;
+    [SerializeField] private GameObject _terminalOnObject;
+    [SerializeField] private GameObject _terminalOffObject;
     void Start()
     {
         _hackingTerminal.CanHack
             .Subscribe(isActive => 
             {
                 if (_objectToToggle != null)
-                {
-                    _objectToToggle.SetActive(isActive); 
-                }
+                    _objectToToggle.SetActive(isActive);
                 
+                if (_terminalOnObject != null)
+                    _terminalOnObject.SetActive(isActive);
+                
+                if (_terminalOffObject != null)
+                    _terminalOffObject.SetActive(!isActive);
+                
+
                 Debug.Log($"Объект изменен, статус терминала: {isActive}");
             })
             .AddTo(this);
