8167990e5864ab40c944dbc85984a29ccd23414a
Ilya Kochev <kochev0987@gmail.com>
2026-04-26 23:15:43 +0300

    Added logic with different abbilities

--- Assets/_Project/Visual/UI/Menus/BattleMenu/BattleScreen.cs
+++ Assets/_Project/Visual/UI/Menus/BattleMenu/BattleScreen.cs
@@ -1,29 +1,61 @@
+using System;
+using System.Collections.Generic;
 using _Project.Scripts.Infrastructure.Gui.Screens;
 using _Project.Scripts.Scenes.Game.Unit;
+using _Project.Scripts.Scenes.Game.Unit._Configs;
 using _Project.Scripts.Scenes.Game.Unit.Components.Health;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Zenject;
 
 namespace _Project.Visual.UI.Menus.BattleMenu
 {
+    [Serializable]
+    public struct AbilityPair
+    {
+        public GameObject PrefabOn;
+        public GameObject PrefabOff;
+        public Image CooldownOverlay;
+        public TMP_Text CooldownText;
+    }
+    [Serializable]
+    public struct AbilityPrefabMapping
+    {
+        [FormerlySerializedAs("AbilityType")] public BotAbilityType BotAbilityType; 
+        public AbilityPair Prefab;
+    }
     public class BattleScreen : BaseScreen
     {
+        
+        
         // [SerializeField] private GameObject _ability;
         [SerializeField] private Slider _healthSlider;
         [SerializeField] private Image _cooldownOverlay;
         [SerializeField] private TMP_Text _cooldownText;
+        [FormerlySerializedAs("_screenMappings")] [SerializeField] private List<AbilityPrefabMapping> _abilityMappings;
+        private Dictionary<BotAbilityType, AbilityPair> _prefabsDictionary;
         private IAbility _currentAbility;
         private Health _currentHealth;
         private IPlayerProvider _playerProvider;
         private CompositeDisposable _unitDisposables = new CompositeDisposable();
+        private AbilityPair _abilityPair;
         
         [Inject]
         public void Construct(IPlayerProvider playerProvider)
         {
             _playerProvider = playerProvider;
+            _prefabsDictionary = new Dictionary<BotAbilityType, AbilityPair>();
+            foreach (var mapping in _abilityMappings)
+            {
+                if (!_prefabsDictionary.ContainsKey(mapping.BotAbilityType))
+                {
+                    _prefabsDictionary.Add(mapping.BotAbilityType, mapping.Prefab);
+                }
+            }
+
         }
         
         private void Start()
@@ -45,21 +77,31 @@ namespace _Project.Visual.UI.Menus.BattleMenu
             if (newUnit == null) return;
             
             var ability = newUnit.Ability; 
-            _currentHealth = newUnit.Health;
             
-            if (ability == null)
-            {
-                Debug.LogWarning($"[BattleScreen] У юнита {newUnit.name} нет абилки!");
-                return;
-            }
+            _abilityPair = _prefabsDictionary[ability.GetAbilityType()];
+            _currentHealth = newUnit.Health;
+            //
+            // _abilityPair.PrefabOn.SetActive(true);
+            
+            Debug.Log($"[BattleScreen] Current _abilityPair {_abilityPair}, PrefabOn {_abilityPair.PrefabOn}, PrefabOff {_abilityPair.PrefabOff}");
             Debug.Log($"[BattleScreen] Подписываемся на абилку. Текущее состояние IsReady: {ability.IsReady.Value}");
+            
             // ability.IsReady
             //     .Subscribe(ready => 
             //     {
             //         Debug.Log($"[BattleScreen] Реакция на изменение IsReady: {ready}");
-            //         
-            //         if(ready)
+            //         if (ready)
+            //         {
             //             _cooldownText.text = $"";
+            //             prefab.PrefabOn.SetActive(true);
+            //             prefab.PrefabOff.SetActive(false);
+            //         }
+            //         else
+            //         {
+            //             prefab.PrefabOn.SetActive(true);
+            //             prefab.PrefabOff.SetActive(false);
+            //         }
+            //             
             //     })
             //     .AddTo(_unitDisposables);
             Observable.EveryUpdate()
@@ -68,21 +110,28 @@ namespace _Project.Visual.UI.Menus.BattleMenu
                     if (ability.MaxCooldown > 0)
                     {
                         float fill = ability.CurrentTimer / ability.MaxCooldown;
-                        if(!ability.IsReady.Value)
-                            _cooldownText.text = $"{ability.CurrentTimer:F1}";
+                        if (!ability.IsReady.Value)
+                        {
+                            
+                            _abilityPair.CooldownText.text = $"{ability.CurrentTimer:F1}";
+                            _abilityPair.PrefabOn.SetActive(false);
+                            _abilityPair.PrefabOff.SetActive(true);
+                        }
                         else
                         {
-                            _cooldownText.text = $"";
+                            _abilityPair.CooldownText.text = $"";
+                            _abilityPair.PrefabOn.SetActive(true);
+                            _abilityPair.PrefabOff.SetActive(false);
                         }
-                        _cooldownOverlay.fillAmount = fill;
+                        _abilityPair.CooldownOverlay.fillAmount = fill;
                     }
                 })
-                .AddTo(_unitDisposables);
+                .AddTo(this);
             _currentHealth.CurrentHealth.Subscribe(_ =>
                 {
                     ChangeHealth();
                 })
-            .AddTo(_unitDisposables);
+            .AddTo(this);
             
         }
         
