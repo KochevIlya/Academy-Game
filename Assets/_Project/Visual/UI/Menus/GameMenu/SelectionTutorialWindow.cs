4ede807c27d6ebe6436784071a0d037a1a41eb49
Yaroslav <yarik130906@gmail.com>
2026-05-08 15:09:35 +0300

    Added skip in hacking tutorial

--- Assets/_Project/Visual/UI/Menus/GameMenu/SelectionTutorialWindow.cs
+++ Assets/_Project/Visual/UI/Menus/GameMenu/SelectionTutorialWindow.cs
@@ -5,12 +5,15 @@ using _Project.Scripts.Infrastructure.Gui.Service;
 using Cysharp.Threading.Tasks;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Zenject;
 
 public class SelectionTutorialWindow : BaseScreen
 {
     private HackingService _hackingService;
     private IGuiGameService _guiService;
+    [SerializeField] private SkipPanel _skipHandler;
+    [SerializeField] private GameObject _tutorPanel;
  
     public override async UniTask Show()
     {
@@ -33,6 +36,14 @@ public class SelectionTutorialWindow : BaseScreen
         _hackingService.OnHackingStarted
             .Subscribe(_ => SwitchWindow())
             .AddTo(LifeTimeDisposable);
+
+        if (_skipHandler != null)
+        {
+            _skipHandler.OnSkipPressed
+                .Subscribe(_ => HideTutor())
+                .AddTo(LifeTimeDisposable);
+        }
+        
         _guiService.CloseScreen(ScreenType.HackingSelectionWindow);
     }
     
@@ -41,6 +52,12 @@ public class SelectionTutorialWindow : BaseScreen
         Time.timeScale = 1f;
         _guiService.ShowWindow(ScreenType.AbilitiesTutorialWindow).Forget();
         _guiService.CloseScreen(ScreenType.SelectionTutorialWindow).Forget();
+    }
+
+    private void HideTutor()
+    {
+        Time.timeScale = 1f;
+        _tutorPanel.SetActive(false);
     }
     
     public override ScreenType GetScreenType()
